d4a4546a5012215c0e464c1069552319b4502923
Matthew Franglen <matthew@franglen.org>
2018-06-05 22:31:37 +0100

    Add image tags to docker-compose
    This means that the build command is only required for the base image,
    which is also available from the hub.

--- bin/build.sh
+++ bin/build.sh
@@ -1,50 +1,18 @@
 #!/bin/bash
 
-COMMAND=`readlink -f ${0}`
-BIN_FOLDER=`dirname "${COMMAND}"`
-. "${BIN_FOLDER}/lib"
+readonly COMMAND=`readlink -f ${0}`
+readonly BIN_FOLDER=`dirname "${COMMAND}"`
+readonly ROOT_FOLDER=`dirname "${BIN_FOLDER}"`
+readonly DOCKER_FOLDER="${ROOT_FOLDER}/docker"
 
 main () {
-    build_base_image                  &&
-        build_api_image               &&
-        build_client_image            &&
-        build_server_image            &&
-        build_standalone_client_image
+    build_base_image
 }
-
 
 build_base_image () {
     (
         cd "${DOCKER_FOLDER}/base"
         docker build --tag matthewfranglen/sensu-base .
-    )
-}
-
-build_api_image () {
-    (
-        cd "${DOCKER_FOLDER}/api"
-        docker build --tag matthewfranglen/sensu-api .
-    )
-}
-
-build_client_image () {
-    (
-        cd "${DOCKER_FOLDER}/client"
-        docker build --tag matthewfranglen/sensu-client .
-    )
-}
-
-build_server_image () {
-    (
-        cd "${DOCKER_FOLDER}/server"
-        docker build --tag matthewfranglen/sensu-server .
-    )
-}
-
-build_standalone_client_image () {
-    (
-        cd "${DOCKER_FOLDER}/standalone-client"
-        docker build --tag matthewfranglen/sensu-standalone-client .
     )
 }
 
