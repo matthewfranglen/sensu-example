950c4e5011789c4801a511f8701d9700a36d8013
Matthew Franglen <matthew@franglen.org>
2018-06-05 23:10:45 +0100

    Simplify build

--- bin/build.sh
+++ bin/build.sh
@@ -5,15 +5,7 @@ readonly BIN_FOLDER=`dirname "${COMMAND}"`
 readonly ROOT_FOLDER=`dirname "${BIN_FOLDER}"`
 readonly DOCKER_FOLDER="${ROOT_FOLDER}/docker"
 
-main () {
-    build_base_image
-}
-
-build_base_image () {
-    (
-        cd "${DOCKER_FOLDER}/base"
-        docker build --tag matthewfranglen/sensu-base .
-    )
-}
-
-main
+(
+    cd "${DOCKER_FOLDER}/base"
+    docker build --tag matthewfranglen/sensu-base "${@}" .
+)
